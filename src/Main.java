6366ac946ae23a0dca7f9f8688053960be88830a
Thailan <thailanrs@gmail.com>
2023-03-03 19:40:19 -0400

    adicionadas as classes de Conteudo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import dio.desafio.dominio.Curso;
+import dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descricao Curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso C#");
+        curso2.setDescricao("Descricao Curso C#");
+        curso2.setCargaHoraria(12);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descricao mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+
+
     }
 }
